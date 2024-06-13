a2099a32cad8030cff492f41fbadcedbaa035e1a
David Siaw <dsiaw@degica.com>
2024-06-13 20:59:38 +0900

    spec

--- spec/mysql2-iamauth_spec.rb
+++ spec/mysql2-iamauth_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Mysql2::Iamauth do
   it 'has a version number' do
     expect(Mysql2::Iamauth::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
